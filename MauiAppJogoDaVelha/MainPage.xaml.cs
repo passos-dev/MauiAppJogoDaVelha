a92fa17f3b75768b3ff6c9b93179893b3443080e
UsuarioX <UsuarioX@DESKTOP-6SKKFSI>
2026-05-25 00:21:23 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace MauiAppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+        //Contador de jogadas para sabermos se deu velha (máximo de 9 cliques)
+        int jogadas = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            btn.IsEnabled = false;
+
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                vez = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+            //Toda vez que um botão for clicado, somamos 1 ao total de jogadas
+            jogadas++;
+
+            if (VerificarVencedor("X"))
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                Zerar();
+            }
+            else if (VerificarVencedor("O"))
+            {
+                DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+                Zerar();
+            }
+            else if (jogadas == 9)
+            {
+                //9 cliques e nenhum "if" acima deu verdadeiro, deu Velha!
+                DisplayAlert("Deu Velha!", "Ninguém ganhou essa rodada.", "OK");
+                Zerar();
+            }
+
+        } // Fecha método do clique
+
+        /* Juiz do Jogo checa as 8 combinações possíveis.
+           Retorna 'true' se o jogador enviado (X ou O) preencheu uma trinca.*/
+        bool VerificarVencedor(string jogador)
+        {
+            //Verificação das Linhas Horizontais
+            if (btn10.Text == jogador && btn11.Text == jogador && btn12.Text == jogador) return true;
+            if (btn20.Text == jogador && btn21.Text == jogador && btn22.Text == jogador) return true;
+            if (btn30.Text == jogador && btn31.Text == jogador && btn32.Text == jogador) return true;
+
+            //Verificação das Linhas Verticais
+            if (btn10.Text == jogador && btn20.Text == jogador && btn30.Text == jogador) return true;
+            if (btn11.Text == jogador && btn21.Text == jogador && btn31.Text == jogador) return true;
+            if (btn12.Text == jogador && btn22.Text == jogador && btn32.Text == jogador) return true;
+
+            //Verificação das Diagonais
+            if (btn10.Text == jogador && btn21.Text == jogador && btn32.Text == jogador) return true;
+            if (btn12.Text == jogador && btn21.Text == jogador && btn30.Text == jogador) return true;
+
+            //Se o código chegou até aqui, significa que esse jogador não fechou nenhuma trinca
+            return false;
+        }
+
+        void Zerar()
+        {
+            // Limpa o texto de todos os 9 Objetos botões
+            btn10.Text = ""; btn11.Text = ""; btn12.Text = "";
+            btn20.Text = ""; btn21.Text = ""; btn22.Text = "";
+            btn30.Text = ""; btn31.Text = ""; btn32.Text = "";
+
+            // Reativa o Atributo IsEnabled de todos para que possam ser clicados de novo
+            btn10.IsEnabled = true; btn11.IsEnabled = true; btn12.IsEnabled = true;
+            btn20.IsEnabled = true; btn21.IsEnabled = true; btn22.IsEnabled = true;
+            btn30.IsEnabled = true; btn31.IsEnabled = true; btn32.IsEnabled = true;
+
+            // Reseta as variáveis de controle do jogo
+            vez = "X";
+            jogadas = 0; // NOVO: Zera as jogadas para começar a contar de novo
+        }
+
+    } // Fecha Classe
+} // Fecha Namespace
---
+++ README.md
@@ -0,0 +1,34 @@
+Repositório do projeto do Jogo da Velha com .NET MAUI usando ```Grid```
+
+## Sobre o curso de Desenvolvimento de Apps com .NET MAUI
+Essa é a segunda aula de 16 do nosso curso de desenvolvimento de aplicativos Android/Windows com .NET MAUI
+- [Videoaula 2 - Treinando entrada, processamento e saída de Dados](https://github.com/tiagotas/MauiAppMeuCombustivel)
+- [Videoaula 4 - Navegação entre telas com Sistema de Login](https://github.com/tiagotas/MauiAppLogin)
+
+## Playlist com todas as videoaulas de Apps com .NET MAUI
+- [https://www.youtube.com/playlist?list=PLHVpcBDJr5dlTaaogjFikUDrw1E6PlODf](https://www.youtube.com/watch?v=HCiwRZm_APo&list=PLHVpcBDJr5dmdDjPw11d9Iqhh0gX_laJR)
+
+## Conteúdo das Videoaulas
+### VIDEOAULA: AULA 3 - CRIANDO APLICATIVOS COM .NET MAUI - JOGO DA VELHA (43mim):
+- 00:00 Introdução
+- 01:11 Criação do Projeto
+- 03:33 Compilando App em Branco
+- 04:40 Estrutura do Projeto
+- 05:37 Editando a interface Gráfica
+- 06:40 Fundo Degradê
+- 09:55 Definição do Layout em ```Grid```
+- 11:43 Colocando ```Label``` na ```Grid```
+- 13:27 Colocando ```Button``` na ```Grid```
+- 15:40 Espaçamento entre Colunas da ```Grid```
+- 16:56 Definindo Estilos no XAML
+- 20:36 Adicionando Eventos no Botão
+- 21:53 Manipulador de Eventos no C#
+- 23:50 Entendo o parâmetro ```Sender```
+- 26:22 Implementando as Regras do Jogo
+- 29:18 Verificando quem Ganhou
+- 35:11 Zerando Jogo após o Término
+- 37:44 Impedindo o Jogador de Escolher uma Posição
+- 40:07 Colocando o Projeto no Github
+
+## CURTIU MEU TRABALHO?
+Você pode me ajudar com um pix para tiago@tiago.blog.br 🍻
